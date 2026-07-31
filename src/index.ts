487859c1b1fa00acdbe62025e7d4ec088c6cecba
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-07-31 06:56:41 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+type Student = {
+    name: string;
+    id: number;
+    age: number;
+    cgpa: number;
+    department: Department;
+    marks: [number, number, number];
+};
+const students: Student[] = [];
+
+enum Department{
+    CS,
+    AI,
+    SE,
+    IT
+}
+
+const st1:Student ={
+    name:"mubashir",
+    id:1,cgpa:3.3,
+    age:21,
+    department:Department.CS,
+    marks:[46,44,77]
+};
+const st2:Student={
+    name:"ali",
+    id:2,
+    cgpa:3.5,
+    age:21,
+    department:Department.SE,
+    marks:[22,33,44]
+};
+const st3:Student={
+    name:"saad",
+    id:3,
+    cgpa:3.2,
+    age:21,
+    department:Department.AI,
+    marks:[22,33,44]
+};
+const st4:Student={
+    name:"asad",
+    id:4,
+    cgpa:3.5,
+    age:21,
+    department:Department.IT,
+    marks:[44,55,66]
+};
+const st5:Student={
+    name:"furqan",
+    id:5,
+    cgpa:3.5,
+    age:21,
+    department:Department.IT,
+    marks:[77,88,99],
+};
+
+function showStudents() {
+    for (const student of students) {
+        console.log(student.name);
+        console.log(student.id);
+        console.log(student.cgpa);
+        console.log(student.age);
+        console.log(student.department);
+        console.log(student.marks);
+    }
+}
+
