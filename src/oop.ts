030f80a01f845ff69e06cf870081aa58ee830c46
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-08-17 08:47:32 -0700

    this key word

--- src/oop.ts
+++ src/oop.ts
@@ -1,9 +1,24 @@
 class student{
     name:string;
     age:number;
+    
  constructor(name: string, age: number) {
         console.log(`I am ${name}, my age is ${age}`);
     }
 }
 
 const str= new student("Ali",21);
+
+
+ class teacher{
+    name:string;
+     age:number;
+
+ constructor(name: string, age: number) {
+        console.log(`I am ${name}, my age is ${age}`);
+        this.name=name;
+        this.age=age;
+   } }
+const tr=new teacher ("saad",44)
+
+console.log(str.name)
