799d00c3e97ac62f14fa914897759342c2ee0982
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-08-17 08:26:23 -0700

    class

---
+++ src/oop.ts
@@ -0,0 +1,9 @@
+class student{
+    name:string;
+    age:number;
+ constructor(name: string, age: number) {
+        console.log(`I am ${name}, my age is ${age}`);
+    }
+}
+
+const str= new student("Ali",21);
