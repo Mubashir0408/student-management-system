fb648b33d0cc8f528f9f7cfe2745e423832dbe0b
Mubashir Ejaz <mubashirejazkhan@gmail.com>
2026-08-22 07:40:06 -0700

    initial

--- src/genericsandinterfaces.ts
+++ src/genericsandinterfaces.ts
@@ -9,3 +9,17 @@ const student1: Student = {
     age: 21,
     cgpa: 3.5
 };
+
+
+const student1: Student = {
+    name: "Asad",
+    age: 21,
+    cgpa: 3.1
+};
+
+
+const student3: Student = {
+    name: "saad",
+    age: 21,
+    cgpa: 3.0
+};
